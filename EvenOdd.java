977eebe4d68042a91f8f49624a23c5b4bb3f28b4
Aditi Kadam <107168845+Adiitiiii@users.noreply.github.com>
2023-09-12 00:07:11 +0530

    Add files via upload
    To check whether the number entered by the user is even or odd.

---
+++ EvenOdd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class EvenOdd{
+    public static void main(String args[]){
+        int n;
+        System.out.println("enter a number");
+        Scanner sc= new Scanner(System.in);
+        n= sc.nextInt();
+        if(n%2==0){
+            System.out.println("Number is even");
+        }
+        else {
+            System.out.println("Number is odd");
+        }
+            
+        
+    }
+    }
+
